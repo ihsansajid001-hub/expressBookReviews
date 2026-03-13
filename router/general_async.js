0e7ede77e23034f2b9cacc5275685158f9322959
ihsansajid001-hub <ihsansajid001@gmail.com>
2026-03-14 05:04:36 +0800

    Update general_async.js to use Axios for HTTP requests

--- router/general_async.js
+++ router/general_async.js
@@ -1,88 +1,57 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 const public_users = express.Router();
 
-// Task 10: Get all books using async callback function
-public_users.get('/', function (req, res) {
-  const getBooks = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(books);
-    }, 1000);
-  });
+const BASE_URL = 'http://localhost:5000';
 
-  getBooks.then((bookList) => {
-    res.send(JSON.stringify(bookList, null, 4));
-  });
+// Task 10: Get all books using async callback function with Axios
+public_users.get('/async', async function (req, res) {
+  try {
+    const response = await axios.get(`${BASE_URL}/`);
+    res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching books", error: error.message });
+  }
 });
 
-// Task 11: Search by ISBN using Promises
-public_users.get('/isbn/:isbn', function (req, res) {
+// Task 11: Search by ISBN using Promises with Axios
+public_users.get('/async/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   
-  const getBookByISBN = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const book = books[isbn];
-      if (book) {
-        resolve(book);
-      } else {
-        reject("Book not found");
-      }
-    }, 1000);
-  });
-
-  getBookByISBN
-    .then((book) => res.send(JSON.stringify(book, null, 4)))
-    .catch((error) => res.status(404).json({ message: error }));
+  axios.get(`${BASE_URL}/isbn/${isbn}`)
+    .then((response) => {
+      res.send(JSON.stringify(response.data, null, 4));
+    })
+    .catch((error) => {
+      res.status(404).json({ message: "Book not found", error: error.message });
+    });
 });
 
-// Task 11: Search by Author using Promises
-public_users.get('/author/:author', function (req, res) {
+// Task 11: Search by Author using Promises with Axios
+public_users.get('/async/author/:author', function (req, res) {
   const author = req.params.author;
   
-  const getBooksByAuthor = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let filtered_books = [];
-      for (let key in books) {
-        if (books[key].author === author) {
-          filtered_books.push(books[key]);
-        }
-      }
-      if (filtered_books.length > 0) {
-        resolve(filtered_books);
-      } else {
-        reject("No books found for this author");
-      }
-    }, 1000);
-  });
-
-  getBooksByAuthor
-    .then((bookList) => res.send(JSON.stringify(bookList, null, 4)))
-    .catch((error) => res.status(404).json({ message: error }));
+  axios.get(`${BASE_URL}/author/${encodeURIComponent(author)}`)
+    .then((response) => {
+      res.send(JSON.stringify(response.data, null, 4));
+    })
+    .catch((error) => {
+      res.status(404).json({ message: "No books found for this author", error: error.message });
+    });
 });
 
-// Task 11: Search by Title using Promises
-public_users.get('/title/:title', function (req, res) {
+// Task 11: Search by Title using Promises with Axios
+public_users.get('/async/title/:title', function (req, res) {
   const title = req.params.title;
   
-  const getBooksByTitle = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let filtered_books = [];
-      for (let key in books) {
-        if (books[key].title === title) {
-          filtered_books.push(books[key]);
-        }
-      }
-      if (filtered_books.length > 0) {
-        resolve(filtered_books);
-      } else {
-        reject("No books found with this title");
-      }
-    }, 1000);
-  });
-
-  getBooksByTitle
-    .then((bookList) => res.send(JSON.stringify(bookList, null, 4)))
-    .catch((error) => res.status(404).json({ message: error }));
+  axios.get(`${BASE_URL}/title/${encodeURIComponent(title)}`)
+    .then((response) => {
+      res.send(JSON.stringify(response.data, null, 4));
+    })
+    .catch((error) => {
+      res.status(404).json({ message: "No books found with this title", error: error.message });
+    });
 });
 
 module.exports.general = public_users;
